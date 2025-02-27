052ee1ffebec9296008eb3b99ad93e59de1e2115
Santosl2 <mfilype2019@gmail.com>
2025-02-27 09:58:03 -0300

    wip

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log("This is a template project");
+console.log(
+  "Make sure to update the package.json file with your project details"
+);
+console.log("Also, update the README.md file with your project details");
+console.log("Happy coding!");
+console.log("🚀");
